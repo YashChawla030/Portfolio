e0e890dfacdd613ddfbb29f28335421cf47e8749
Aabhishek29 <starkabhishek29@gmail.com>
2023-01-18 22:30:32 +0530

    new resume added

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,7 +6,20 @@ function Home (){
 	return( 
 		<div className='middle'>
 			<div className='intro-div'>
-			<header className='header-title'>Hello, I'm</header>
+				<div className='intro-title'>
+					<h1 style={{'fontSize':18}} className='header-title'>Hello, I'm</h1>
+				</div>
+				<div className='intro-title'>
+					<h1 style={{'fontSize':28}} className='header-title'>Abhishek Singh</h1>
+				</div>
+				<div className='intro-title'>
+					<h1 style={{'fontSize':22}} className='header-title'>Android Developer And iOS Developer</h1>
+				</div>
+				<div className='intro-title'>
+					<p className='discription'>A web developer is a programmer who develops World Wide Web 
+						applications using a client–server model. The applications 
+						typically use HTML, CSS, and JavaScript in the client.</p>
+				</div>
 			</div>
 		</div>
 	)
